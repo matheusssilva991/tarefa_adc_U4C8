03fc2c5ebfb8878e16a5d4c02692ab550bcf3df2
matheusssilva991 <matheus.ssilva991@gmail.com>
2025-02-11 10:02:19 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/adc.h"
+
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define ENDERECO 0x3C
+#define GREEN_LED_PIN 11
+#define BLUE_LED_PIN 12
+#define RED_LED_PIN 13
+#define BTN_A_PIN 5
+#define VRX_PIN 27    // Define o pino GP26 para o eixo X do joystick (Canal ADC0).
+#define VRY_PIN 26    // Define o pino GP27 para o eixo Y do joystick (Canal ADC1).
+#define SW_PIN 22     // Define o pino GP22 para o botão do joystick (entrada digital).
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+
+void init_led(uint8_t led_pin);
+void init_btn(uint8_t btn_pin);
+void init_joystick();
+void init_i2c();
+void init_display(ssd1306_t *ssd);
+
+ssd1306_t ssd; // Inicializa a estrutura do display
+
+int main()
+{
+
+    stdio_init_all();
+
+    // Inicializa os LEDs
+    init_led(GREEN_LED_PIN);
+    init_led(BLUE_LED_PIN);
+    init_led(RED_LED_PIN);
+
+    // Inicializa os botões
+    init_btn(BTN_A_PIN);
+
+    // Inicializa a comunicação I2C
+    init_i2c();
+
+    // Inicializa o display
+    init_display(&ssd);
+
+    // Inicializa o ADC e o Joystick
+    adc_init();
+    init_joystick();
+
+    uint16_t adc_value_x;
+    uint16_t adc_value_y;
+    char str_x[5];  // Buffer para armazenar a string
+    char str_y[5];  // Buffer para armazenar a string
+    bool color = true;
+
+    while (true)
+    {
+        adc_select_input(1); // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
+        adc_value_x = adc_read();
+
+        adc_select_input(0); // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
+        adc_value_y = adc_read();
+
+        sprintf(str_x, "%d", adc_value_x);  // Converte o inteiro em string
+        sprintf(str_y, "%d", adc_value_y);  // Converte o inteiro em string
+
+        // Leitura do estado do botão do joystick (SW)
+        bool sw_value = gpio_get(SW_PIN) == 0; // 0 indica que o botão está pressionado.
+
+        float tmp_x = DISPLAY_WIDTH * (float) adc_value_x / 4096;
+        float tmp_y = DISPLAY_HEIGHT * (1 - (float) adc_value_y / 4096);
+        tmp_x = tmp_x > DISPLAY_WIDTH - 8 ? DISPLAY_WIDTH - 8 : tmp_x;
+        tmp_y = tmp_y > DISPLAY_HEIGHT - 8 ? DISPLAY_HEIGHT - 8 : tmp_y;
+
+        // Imprime os valores lidos na comunicação serial.
+        printf("VRX: %f, VRY: %f, SW: %d\n", tmp_x,  tmp_y, sw_value);
+
+        //ssd1306_draw_string(&ssd, "X, Y");
+        ssd1306_fill(&ssd, !color); // Limpa o display
+        ssd1306_rect(&ssd, (int) tmp_y, (int) tmp_x, 8, 8, color, true); // Desenha um retângulo
+        //ssd1306_draw_string(&ssd, "Teste", 60, 28);
+        ssd1306_send_data(&ssd); // Atualiza o display
+
+        sleep_ms(200);
+    }
+}
+
+// Inicializa um led em um pino específico
+void init_led(uint8_t led_pin)
+{
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+}
+
+// Inicializa um botão em um pino específico
+void init_btn(uint8_t btn_pin)
+{
+    gpio_init(btn_pin);
+    gpio_set_dir(btn_pin, GPIO_IN);
+    gpio_pull_up(btn_pin);
+}
+
+// Inicializa a comunicação I2C
+void init_i2c()
+{
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+}
+
+// Inicializa o display OLED
+void init_display(ssd1306_t *ssd)
+{
+    ssd1306_init(ssd, WIDTH, HEIGHT, false, ENDERECO, I2C_PORT);
+    ssd1306_config(ssd);
+    ssd1306_send_data(ssd);
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(ssd, false);
+    ssd1306_send_data(ssd);
+}
+
+void init_joystick() {
+    adc_gpio_init(VRX_PIN);
+    adc_gpio_init(VRY_PIN);
+
+    init_btn(SW_PIN);
+}
