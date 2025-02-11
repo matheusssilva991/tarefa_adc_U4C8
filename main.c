9328cfbf122d26b3bfd83dcb940afc636d57425d
matheusssilva991 <matheus.ssilva991@gmail.com>
2025-02-11 16:26:24 -0300

    feat: add pwm feat: change leds to pwm feat: add turn on/off pwm for buttons A press

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "pico/bootrom.h"
 #include "hardware/i2c.h"
 #include "hardware/adc.h"
+#include "hardware/pwm.h"
 
 #include "lib/ssd1306.h"
 #include "lib/font.h"
@@ -22,33 +23,43 @@
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 64
 #define ADC_MAX_VALUE 4096
+#define ADC_MIDDLE_VALUE ADC_MAX_VALUE / 2
 #define RECT_XY_SHAPE 8
+#define VRXY_VALUE_ERROR 100
 
-void init_led(uint8_t led_pin);
+uint init_led_pwm(uint8_t led_pin);
 void init_btn(uint8_t btn_pin);
 void init_joystick();
 void init_i2c();
 void init_display(ssd1306_t *ssd);
 void update_joystick_xy_values(uint16_t *x_value, uint16_t *y_value);
+void process_joystick_xy_values(uint16_t x_value_raw, uint16_t y_value_raw, float *x_value, float *y_value);
 void gpio_irq_handler(uint gpio, uint32_t events);
 
-static volatile bool adc_sw_value = true;
+static volatile bool sw_value = true;
 static volatile uint32_t last_time_btn_a = 0; // Armazena o tempo do último evento do btn A (em microssegundos)
 static volatile uint32_t last_time_btn_sw = 0; // Armazena o tempo do último evento do btn B(em microssegundos)
+static volatile uint green_led_slice;
+static volatile uint blue_led_slice;
+static volatile uint red_led_slice;
+static volatile bool is_leds_pwm_enabled = false;
+static volatile bool is_green_led_on = false;
 
 int main()
 {
-    uint16_t adc_x_value;
-    uint16_t adc_y_value;
+    uint16_t vrx_value_raw;
+    uint16_t vry_value_raw;
+    float vrx_value_pos; // Valor de X após processamento
+    float vry_value_pos; // Valor de Y após processamento
     ssd1306_t ssd; // Inicializa a estrutura do display
     bool color = true;
 
     stdio_init_all();
 
     // Inicializa os LEDs
-    init_led(GREEN_LED_PIN);
-    init_led(BLUE_LED_PIN);
-    init_led(RED_LED_PIN);
+    green_led_slice = init_led_pwm(GREEN_LED_PIN);
+    blue_led_slice = init_led_pwm(BLUE_LED_PIN);
+    red_led_slice = init_led_pwm(RED_LED_PIN);
 
     // Inicializa os botões
     init_btn(BTN_A_PIN);
@@ -70,31 +81,35 @@ int main()
 
     while (true)
     {
-        update_joystick_xy_values(&adc_x_value, &adc_y_value);
-
-        float tmp_x = DISPLAY_WIDTH * (float)adc_x_value / ADC_MAX_VALUE;
-        float tmp_y = DISPLAY_HEIGHT * (1 - (float)adc_y_value / ADC_MAX_VALUE);
-        tmp_x = tmp_x > DISPLAY_WIDTH - RECT_XY_SHAPE ? DISPLAY_WIDTH - RECT_XY_SHAPE : tmp_x;
-        tmp_y = tmp_y > DISPLAY_HEIGHT - RECT_XY_SHAPE ? DISPLAY_HEIGHT - RECT_XY_SHAPE : tmp_y;
+        update_joystick_xy_values(&vrx_value_raw, &vry_value_raw);
+        process_joystick_xy_values(vrx_value_raw, vry_value_raw, &vrx_value_pos, &vry_value_pos);
 
         // Imprime os valores lidos na comunicação serial.
-        printf("VRX: %f, VRY: %f, SW: %d\n", tmp_x, tmp_y, adc_sw_value);
+        printf("VRX: %u, VRY: %u, SW: %d\n", vrx_value_raw, vry_value_raw, sw_value);
 
-        ssd1306_fill(&ssd, !adc_sw_value);
-        ssd1306_rect(&ssd, 0, 0, 128, 64, adc_sw_value, !adc_sw_value); // Desenha um retângulo
+        /* bool condition;
+        if (vrx_value_raw > ADC_MIDDLE_VALUE - VRXY_VALUE_ERROR && vrx_value_raw < ADC_MIDDLE_VALUE + VRXY_VALUE_ERROR) {
+
+        } */
+
+        ssd1306_fill(&ssd, !sw_value);
+        ssd1306_rect(&ssd, 0, 0, 128, 64, sw_value, !sw_value); // Desenha um retângulo
                                                                // Limpa o display
-        ssd1306_rect(&ssd, (int)tmp_y, (int)tmp_x, RECT_XY_SHAPE, RECT_XY_SHAPE, color, true); // Desenha um retângulo
+        ssd1306_rect(&ssd, (int)vry_value_pos, (int)vrx_value_pos, RECT_XY_SHAPE, RECT_XY_SHAPE, color, true); // Desenha um retângulo
         ssd1306_send_data(&ssd); // Atualiza o display
 
-        sleep_ms(200);
+        sleep_ms(100);
     }
 }
 
 // Inicializa um led em um pino específico
-void init_led(uint8_t led_pin)
+uint init_led_pwm(uint8_t led_pin)
 {
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
+    gpio_set_function(led_pin, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(led_pin);
+    pwm_set_wrap(slice, ADC_MAX_VALUE);
+    pwm_set_enabled(slice, true);
+    return slice;
 }
 
 // Inicializa um botão em um pino específico
@@ -127,6 +142,7 @@ void init_display(ssd1306_t *ssd)
     ssd1306_send_data(ssd);
 }
 
+// Inicializa o joystick
 void init_joystick()
 {
     adc_gpio_init(VRX_PIN);
@@ -135,6 +151,7 @@ void init_joystick()
     init_btn(SW_PIN);
 }
 
+// Realiza a amostragem e atualiza os valores X e Y do joystick
 void update_joystick_xy_values(uint16_t *x_value, uint16_t *y_value)
 {
     adc_select_input(1); // Seleciona o ADC para eixo X. O pino 26 como entrada analógica
@@ -144,6 +161,15 @@ void update_joystick_xy_values(uint16_t *x_value, uint16_t *y_value)
     *y_value = adc_read();
 }
 
+// Processa os valores de X e Y para a posição correto no display
+void process_joystick_xy_values(uint16_t x_value_raw, uint16_t y_value_raw, float *x_value, float *y_value) {
+    *x_value = DISPLAY_WIDTH * (float) x_value_raw / ADC_MAX_VALUE;
+    *y_value = DISPLAY_HEIGHT * (1 - (float)y_value_raw / ADC_MAX_VALUE);
+    *x_value = *x_value > DISPLAY_WIDTH - RECT_XY_SHAPE ? DISPLAY_WIDTH - RECT_XY_SHAPE : *x_value;
+    *y_value = *y_value > DISPLAY_HEIGHT - RECT_XY_SHAPE ? DISPLAY_HEIGHT - RECT_XY_SHAPE : *y_value;
+}
+
+// Função para manipular os eventos dos botões
 void gpio_irq_handler(uint gpio, uint32_t events) {
     uint32_t current_time = to_us_since_boot(get_absolute_time());
 
@@ -156,15 +182,33 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
         // Atualiza o tempo do último evento do botão A.
         last_time_btn_a = current_time;
 
+        pwm_set_gpio_level(GREEN_LED_PIN, 0);
+        pwm_set_gpio_level(BLUE_LED_PIN, 0);
+        pwm_set_gpio_level(RED_LED_PIN, 0);
+
+        pwm_set_enabled(green_led_slice, !is_leds_pwm_enabled);
+        pwm_set_enabled(blue_led_slice, !is_leds_pwm_enabled);
+        pwm_set_enabled(red_led_slice, !is_leds_pwm_enabled);
+
+        is_leds_pwm_enabled = !is_leds_pwm_enabled;
     }
     else if (gpio == SW_PIN && (current_time - last_time_btn_sw > 200000))
     {
         // Atualiza o tempo do último evento do botão B.
         last_time_btn_sw = current_time;
 
-        adc_sw_value = !adc_sw_value;
+        sw_value = !sw_value;
 
-        gpio_put(GREEN_LED_PIN, !gpio_get(GREEN_LED_PIN));
+        if (is_green_led_on)
+        {
+            pwm_set_gpio_level(GREEN_LED_PIN, 0);
+            is_green_led_on = false;
+        }
+        else
+        {
+            pwm_set_gpio_level(GREEN_LED_PIN, ADC_MIDDLE_VALUE);
+            is_green_led_on = true;
+        }
 
     }
 
